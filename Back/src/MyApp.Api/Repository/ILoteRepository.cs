02c37a6a9f8667f895ae7bd4234c4a4a9994d2a5
MatheusPedracone <matheuspedracone@gmail.com>
2021-11-29 10:56:08 -0300

    creating Lote Repository

---
+++ Back/src/MyApp.Api/Repository/ILoteRepository.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyApp.Api.Models;
+
+namespace MyApp.Api.Repository
+{
+    public interface ILoteRepository
+    {
+
+        /// <summary>
+        /// Método get que ira retornar uma lista de lotes de um evento
+        /// </summary>
+        /// <param name="eventoId"></param>
+        /// <returns>uma array de lotes</returns>
+        Task<Lote[]> GetLotesByEventoIdAsync(int eventoId);
+
+        /// <summary>
+        /// Método get que retornará 1 lote 
+        /// </summary>
+        /// <param name="eventoId"></param>
+        /// <param name="id"></param>
+        /// <returns>1 lote</returns>
+        Task<Lote> GetLoteByIdAsync(int eventoId, int id);
+    }
+}
